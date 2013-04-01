d50af0fdad2d9cc170dbb84ba9e747649d4162cf
Nils Berg <berg.nils@gmail.com>
2013-04-01 05:41:07 +0200

    implemented acronym lookup functionality. Recursion coming soon.

--- err-arrrrr-orrrrr.py
+++ err-arrrrr-orrrrr.py
@@ -1,66 +1,30 @@
 # This is a skeleton for Err plugins, use this to get started quickly.
 
 from errbot import BotPlugin, botcmd
-from errbot.builtins.webserver import webhook
+import re
+import requests
+import xml.etree.ElementTree as etree
 
-
-class Skeleton(BotPlugin):
-	"""An Err plugin skeleton"""
+class Orrrrr(BotPlugin):
+	"""Orrrrr recursive^3 recursor"""
 	min_err_version = '1.6.0' # Optional, but recommended
 	max_err_version = '2.0.0' # Optional, but recommended
-
-	def activate(self):
-		"""Triggers on plugin activation
-
-		You should delete it if you're not using it to override any default behaviour"""
-		super(Skeleton, self).activate()
-
-	def deactivate(self):
-		"""Triggers on plugin deactivation
-
-		You should delete it if you're not using it to override any default behaviour"""
-		super(Skeleton, self).deactivate()
-
-	def get_configuration_template(self):
-		"""Defines the configuration structure this plugin supports
-
-		You should delete it if your plugin doesn't use any configuration like this"""
-		return {'EXAMPLE_KEY_1': "Example value",
-		        'EXAMPLE_KEY_2': ["Example", "Value"]
-		       }
-
-	def check_configuration(self, configuration):
-		"""Triggers when the configuration is checked, shortly before activation
-
-		You should delete it if you're not using it to override any default behaviour"""
-		super(Skeleton, self).check_configuration()
-
-	def callback_connect(self):
-		"""Triggers when bot is connected
-
-		You should delete it if you're not using it to override any default behaviour"""
-		pass
-
-	def callback_message(self, conn, message):
-		"""Triggered for every received message that isn't coming from the bot itself
-
-		You should delete it if you're not using it to override any default behaviour"""
-		pass
-
-	def callback_botmessage(self, message):
-		"""Triggered for every message that comes from the bot itself
-
-		You should delete it if you're not using it to override any default behaviour"""
-		pass
-
-	@webhook
-	def example_webhook(self, incoming_request):
-		"""A webhook which simply returns 'Example'"""
-		return "Example"
 
 	# Passing split_args_with=None will cause arguments to be split on any kind
 	# of whitespace, just like Python's split() does
 	@botcmd(split_args_with=None)
-	def example(self, mess, args):
-		"""A command which simply returns 'Example'"""
-		return "Example"
+	def orrrrr(self, mess, args):
+		"""A command that looks up acronyms at the Internet Acronym Server ( http://acronyms.silmaril.ie )"""
+		p = re.compile('.*[^a-zA-z\\.].*')
+		if len(args) > 1:
+			return "Sorry, acronyms may not contain spaces."
+		elif p.match(args[0]):
+			return "Sorry, nothing but letters and dots is allowed in an acronym."
+		else:
+			r = requests.get("http://acronyms.silmaril.ie/cgi-bin/uncgi/xaa?" + args[0].strip("."))
+			element = etree.fromstring(r.text)
+			if int(element.find('found').attrib['n']) > 0:
+				return next(element.iter('acro')).find('expan').text
+			else:
+				return "Didn't find any definitions, sorry."
+		return "Whoa, something broke O_O"
