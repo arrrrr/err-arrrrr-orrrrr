0f94b99dae9f65085b48d9695ad4a4dc92822490
Nils Berg <berg.nils@gmail.com>
2014-01-09 17:32:46 +0100

    trade delicious recursion for... efficiency? Maybe?
    Also, new layout!

--- err-arrrrr-orrrrr.py
+++ err-arrrrr-orrrrr.py
@@ -21,16 +21,9 @@ class Orrrrr(BotPlugin):
 		elif p.match(args[0]):
 			return "Sorry, nothing but letters and dots is allowed in an acronym."
 		else:
-			r = requests.get("http://acronyms.silmaril.ie/cgi-bin/uncgi/xaa?" + args[0].strip("."))
-			element = etree.fromstring(r.text)
-			if int(element.find('found').attrib['n']) > 0:
-				expanded = next(element.iter('acro')).find('expan').text
-				self.send(mess.getFrom(), expanded, message_type=mess.getType())
-				for word in expanded.split():
-					self.lookup(word, mess.getFrom(), mess.getType(), 0)
-				return "Done."
-			else:
-				return "Didn't find any definitions, sorry."
+			self.send(mess.getFrom(), "Thinking...", message_type=mess.getType())
+			self.lookup_stack(args[0], mess.getFrom(), mess.getType())
+			return
 		return "Whoa, something broke O_O"
 		
 	def lookup(self, term, fromwho, type, depth):
@@ -44,3 +37,29 @@ class Orrrrr(BotPlugin):
 					self.lookup(word, fromwho, type, depth+1)
 		else:
 			self.send(fromwho, "Reached recursion limit.", message_type=type)
+
+	def lookup_stack(self, term, fromwho, type):
+		depth = 0
+		words = []
+		back_in = [1]
+		words.append([term])
+		ret = "Results:"
+		while len(words) > 0:
+			current = words[-1]
+			if len(current) == 0:
+				words.pop() # can't do this after processing the last word in the group ir depth counting (and thus, formatting) would be off
+				continue
+		
+			while len(current) > 0:
+				word = current.pop(0) # need the first, not the last, element
+				ret += "\n" + "\t" * (len(words) - 1) +  word + ": "
+				r = requests.get("http://acronyms.silmaril.ie/cgi-bin/uncgi/xaa?" + word.strip(".()"))
+				element = etree.fromstring(r.text)
+				if int(element.find('found').attrib['n']) > 0 and len(words) < 5:
+					expanded = next(element.iter('acro')).find('expan').text
+					ret += expanded
+					words.append(expanded.split())
+					break # leave the rest of this group until after the new group is processed
+				else:
+					ret += "-"
+		self.send(fromwho, ret, message_type=type)
