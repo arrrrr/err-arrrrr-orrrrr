8c2f815ba5e0c0649ea58d16101603c2272eef2f
Nils Berg <berg.nils@gmail.com>
2014-01-09 18:20:41 +0100

    add !acro command

--- err-arrrrr-orrrrr.py
+++ err-arrrrr-orrrrr.py
@@ -15,23 +15,50 @@ class Orrrrr(BotPlugin):
 	@botcmd(split_args_with=None)
 	def orrrrr(self, mess, args):
 		"""A command that looks up acronyms at the Internet Acronym Server ( http://acronyms.silmaril.ie )"""
-		p = re.compile('.*[^a-zA-z\\.].*')
 		if len(args) > 1:
 			return "Sorry, acronyms may not contain spaces."
-		elif p.match(args[0]):
+		elif not self.input_valid(args[0]):
 			return "Sorry, nothing but letters and dots is allowed in an acronym."
 		else:
 			self.send(mess.getFrom(), "Thinking...", message_type=mess.getType())
 			self.lookup_stack(args[0], mess.getFrom(), mess.getType())
 			return
 		return "Whoa, something broke O_O"
-		
+	
+	def input_valid(self, input):
+		p = re.compile('.*[^a-zA-z\\.].*')
+		return not p.match(input)
+
+	@botcmd(split_args_with=None)
+	def acro(self, mess, args):
+		if len(args) > 1:
+			return "Sorry, acronyms may not contain spaces."
+		elif not self.input_valid(args[0]):
+			return "Sorry, nothing but letters and dots is allowed in an acronym."
+		else:
+			ret = args[0] + ": "
+			expanded = self.lookup_acro(args[0])
+			if len(expanded) > 0:
+				ret += expanded
+			else:
+				ret += "-"
+		return ret
+
+
+	def lookup_acro(self, term):
+		r = requests.get("http://acronyms.silmaril.ie/cgi-bin/uncgi/xaa?" + term.strip(".()"))
+		element = etree.fromstring(r.text)
+		if int(element.find('found').attrib['n']) > 0:
+			expanded = next(element.iter('acro')).find('expan').text
+			return expanded
+		else:
+			return ""
+			
+	
 	def lookup(self, term, fromwho, type, depth):
 		if depth < 5: # constant for now, might add config parameter later
-			r = requests.get("http://acronyms.silmaril.ie/cgi-bin/uncgi/xaa?" + term.strip(".()"))
-			element = etree.fromstring(r.text)
-			if int(element.find('found').attrib['n']) > 0:
-				expanded = next(element.iter('acro')).find('expan').text
+			expanded = self.lookup_acro(term.strip(".()"))
+			if len(expanded) > 0:
 				self.send(fromwho, expanded, message_type=type)
 				for word in expanded.split():
 					self.lookup(word, fromwho, type, depth+1)
@@ -53,10 +80,9 @@ class Orrrrr(BotPlugin):
 			while len(current) > 0:
 				word = current.pop(0) # need the first, not the last, element
 				ret += "\n" + "\t" * (len(words) - 1) +  word + ": "
-				r = requests.get("http://acronyms.silmaril.ie/cgi-bin/uncgi/xaa?" + word.strip(".()"))
-				element = etree.fromstring(r.text)
-				if int(element.find('found').attrib['n']) > 0 and len(words) < 5:
-					expanded = next(element.iter('acro')).find('expan').text
+				
+				expanded = self.lookup_acro(word.strip(".()"))
+				if len(expanded) > 0 and len(words) < 5:
 					ret += expanded
 					words.append(expanded.split())
 					break # leave the rest of this group until after the new group is processed
